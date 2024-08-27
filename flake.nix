a9f8ed3a169d31a5711ce6073ed6530185983060
lofiwatercat <alan001tk@gmail.com>
2024-08-27 00:59:21 -0700

    Update main nix file

--- flake.nix
+++ flake.nix
@@ -1,7 +1,37 @@
 {
   description = "Nix flake dev environments";
 
-  outputs = { self }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+  {
+    devShells = forAllSystems ({ pkgs }:
+      let
+        common = with pkgs; [
+          bash
+          git
+        ];
+      in
+    {
+      # default = pkgs.mkShell {
+      #   packages = common;
+      # };
+      rust = pkgs.mkShell {};
+    });
     templates = {
       rust = {
         path = ./rust;
